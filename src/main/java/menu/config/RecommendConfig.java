50e1264b9c5d5e1d77ac88365323bc03fc103249
youngjunkim <kyj0703@konkuk.ac.kr>
2023-11-24 14:40:29 +0900

    feat: 시작 문구 출력 기능 추가

--- src/main/java/menu/config/RecommendConfig.java
+++ src/main/java/menu/config/RecommendConfig.java
@@ -1,10 +1,20 @@
 package menu.config;
 
 import menu.controller.RecommendController;
+import menu.view.InputView;
+import menu.view.OutputView;
 
 public class RecommendConfig {
     public static RecommendController getRecommendController(){
 
-        return null;
+        return new RecommendController(getInputView(), getOutputView());
+    }
+
+    private static InputView getInputView(){
+        return new InputView();
+    }
+
+    private static OutputView getOutputView(){
+        return new OutputView();
     }
 }
--- src/main/java/menu/controller/RecommendController.java
+++ src/main/java/menu/controller/RecommendController.java
@@ -1,8 +1,20 @@
 package menu.controller;
 
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class RecommendController {
 
+    private InputView inputView;
+    private OutputView outputView;
+
+    public RecommendController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
     public void run() {
+        outputView.printStartMessage();
         input();
         output();
     }
